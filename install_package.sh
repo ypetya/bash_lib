d4c0d3375188d95f38fc3e21c383bf53a876c986
Peter Kiss <ypetya@gmail.com>
2016-05-10 19:14:30 +0200

    disable annoying automatic install on linux

--- install_package.sh
+++ install_package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 import is_linux ask_user
+
+:${AUTO_INSTALL:0}
 # TODO add install instructions for mingw
 function install_package() {
-	if is_linux ; then
+	if is_linux && [ "$AUTO_INSTALL" == "1" ]; then
 		local prog="${1? param missing - please specify package name}"
 		if ask_user "Install $prog?" ; then
 			if sudo apt-get install "$prog" ; then return 0 ; fi
