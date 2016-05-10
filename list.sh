3a93ee9416190396094085edf05a875ba44458ed
Peter Kiss <ypetya@gmail.com>
2016-05-10 10:44:30 +0200

    improve list : maxdepth 1 - disallow testing to appear in interactive

--- list.sh
+++ list.sh
@@ -3,5 +3,5 @@
 # list all the commands availabe for require
 function list() {
   get_source_dir DIR
-  find $DIR -iname '*.sh' | sed 's|'$DIR'/||' | sed 's|.sh$||'
+  find $DIR  -maxdepth 1 -iname '*.sh' | sed 's|'$DIR'/||' | sed 's|.sh$||'
 }
