d6705727486332385adead2356686b57c16fbd31
Peter Kiss <ypetya@gmail.com>
2016-05-11 09:03:01 +0200

    fix git_changed

--- git_changed.sh
+++ git_changed.sh
@@ -3,7 +3,7 @@
 import git_get_changes_number
 
 function git_changed() {
-	if [ git_get_changes_number == "0" ] ; then
+	if [ "$(git_get_changes_number)" == "0" ] ; then
 		return 1 # returning false when no change
 	fi
 	
