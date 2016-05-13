4c9118742845156904a21ac9210fcbcb83f5da33
Peter Kiss <ypetya@gmail.com>
2016-05-13 17:11:14 +0200

    add git_get_commits_count

---
+++ git_get_commits_count.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+import to_lower list_counts_desc
+
+function git_get_commits_count() {
+	#git log --pretty=oneline | wc -l
+	git log --pretty=format:"%aE" | to_lower | list_counts_desc
+}
