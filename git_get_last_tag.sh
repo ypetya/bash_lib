896ad3c8f4ba030af108ef1fe54cabb07cbaca45
Peter Kiss <ypetya@gmail.com>
2016-05-17 09:30:57 +0200

    fix git tags
    * return the real latest tag by the correct ordering

--- git_get_last_tag.sh
+++ git_get_last_tag.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-import tags last
+import tags first
 
 function git_get_last_tag() {
-	tags | last
+	# you can apply more sort=<key> clauses for git tag
+	# the last one gets to be the primary key, or overrides the prev.
+	tags --sort="-version:refname" | first
 }
--- tags.sh
+++ tags.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function tags() {
-	git tag -l $@
+	git tag -l --sort="version:refname" $@
 }
