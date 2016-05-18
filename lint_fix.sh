983fd2109773518c8de767bc761a92cf293b14c9
Peter Kiss <ypetya@gmail.com>
2016-05-18 18:47:29 +0200

    lint_fix messages fix

--- lint_fix.sh
+++ lint_fix.sh
@@ -12,11 +12,11 @@ function lint_fix () {
 		if ! grunt eslint:dev ; then
 			error ' * eslint failed, executing eslint fix'
 			npm run eslint-fix
-			print yellow " * automatic fixes have been applied!\n"
+			print yellow " * automatic fixes have been applied\n"
 			if grunt eslint:dev ; then
-				print green ok
+				print green " * ok\n"
 			else
-				error 'there is still an error, which can not have been resolved.'
+				error " * there is still an error, which can not have been resolved.\n"
 				return 1
 			fi
 		fi
