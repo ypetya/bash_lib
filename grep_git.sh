b25c01aa0764f8b2b64a8a4d8879870513ee48f5
Peter Kiss <ypetya@gmail.com>
2016-05-18 15:34:22 +0200

    fix grep_git

--- grep_git.sh
+++ grep_git.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-import print git_get_committers_of_file
+import print git_get_committers_of_file list_get_row
 
 # TODO rename it grep_blame
 function grep_git() {
 	local file_name
-	local last_file_name
+	local last_file_name='-1'
 	local change_line
 	local matching_line
 	local committers
-	local committer
 	# -i insensitive
 	# -r recursive
 	# -n containing line numbers
@@ -18,14 +17,14 @@ function grep_git() {
 		file_name="$(echo $match | cut -d ':' -f 1)"
 		change_line="$(echo $match | cut -d ':' -f 2)"
 		matching_line="$(echo $match | cut -d ':' -f 3,4,5)"
-		if [ ! "$file_name"="$last_file_name" ] ; then
-			committers=$(git_get_committers_of_file "$file_name")
+		if [ ! "$file_name" == "$last_file_name" ] ; then
+			committers=( $(git_get_committers_of_file "$file_name") )
 		fi
-		committer=$( echo $committers | sed -n "${change_line}p" )
+		
 		last_file_name="$file_name"
 		print yellow "$file_name"
 		print ":$change_line "
-		print green "$committer"
+		print green "${committers[$change_line]}"
 		print ' '
 		echo "$matching_line"
 	done
--- list_get_row.sh
+++ list_get_row.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function list_get_row() {
-	local row_num={$1? param missing : row_num}
+	local row_num=${1? param missing : row_num}
 	sed -n "${row_num}d"
 }
