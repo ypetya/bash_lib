aff7280235f5ab19680c683551a16fd30f75a9dd
Peter Kiss <ypetya@gmail.com>
2016-05-19 16:51:37 +0200

    introduce list_files_not_contain helper function

---
+++ file_contains.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function file_contains() {
+	local result=$( 
+		sed -nr "s/${1? param missing : string to find}/1/p" "${2? param missing - file}" | wc -l
+	)
+	if (( result == 0 )) ; then
+		return 1
+	fi
+}
