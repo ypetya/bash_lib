95074a15c0e5e664f42e6786d089d22554a0d328
Peter Kiss <ypetya@gmail.com>
2016-05-20 15:23:58 +0200

    add fix_json

---
+++ fix_json.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+import ask_user
+# handy helper fn to fix json formatting:
+function fix_json() {
+	local file="${1? param missing - file}"
+	if ask_user "It will overwrite $file. Are you sure?" ; then
+		# 1.: change "'" -> "\""
+		sed -ri "s/[']/\"/g" "$file"
+		# 2. quoted keys
+		sed -ri 's/([a-zA-Z]+):/"\1":/' "$file"
+	fi
+}
