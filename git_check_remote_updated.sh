a2f9e4a89296ec13ad93927cc800ec0d7ba4287c
Peter Kiss <ypetya@gmail.com>
2016-05-23 18:09:07 +0200

    introduce git_check_remote_updated

---
+++ git_check_remote_updated.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function git_check_remote_updated() {
+	local remote_url="${1? param missing - remote_url}"
+	
+	local remote_sha1="$(git ls-remote "$remote_url" | grep HEAD | cut -f 1)"
+	debug "remote is : $remote_sha1"
+	local local_sha1="$(git log -1 --pretty=format:"%H")"
+	debug "origin is : $local_sha1"
+	
+	if [ "$remote_sha1" == "$local_sha1" ] ; then
+		return 1
+	fi
+}
