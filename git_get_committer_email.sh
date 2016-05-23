846c35f96f16d219bfe88f51715549f537675c1e
Peter Kiss <ypetya@gmail.com>
2016-05-23 10:44:56 +0200

    git committer email

--- git_get_committer_email.sh
+++ git_get_committer_email.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+import is_variable
+
 function git_get_committer_email() {
-	echo $(git config --get user.email || echo $GIT_COMMITTER_EMAIL)
+	# priority 1.
+	if is_variable GIT_COMMITTER_EMAIL ; then
+		echo "$GIT_COMMITTER_EMAIL"
+	else
+		git config --get user.email
+	fi
 }
