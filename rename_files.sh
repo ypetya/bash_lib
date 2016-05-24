cd06f3040865885e552c3b7b08b90c4b592e3e16
Peter Kiss <ypetya@gmail.com>
2016-05-24 15:10:58 +0200

    rename_files : is for mass renaming

---
+++ rename_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function rename_files() {
+	local change_from="${1? param missing - change_from}"
+	local change_to="${2? param missing - change_to}"
+
+	for f in $( find . ) ; do
+		print red "$f"
+		print " -> "
+		print green "${f//$change_from/$change_to}\n"
+	done
+		
+	if ask_user 'Is this correct?' ; then
+		for f in $( find . ) ; do 
+			mv -v "$f" "${f//$change_from/$change_to}"
+		done
+	fi
+	
+}
