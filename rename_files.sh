378c321b609703be8dac5723d818de014f2d9c93
Peter Kiss <ypetya@gmail.com>
2016-05-24 18:11:08 +0200

    fix rename_files

--- rename_files.sh
+++ rename_files.sh
@@ -4,14 +4,16 @@ function rename_files() {
 	local change_from="${1? param missing - change_from}"
 	local change_to="${2? param missing - change_to}"
 
-	for f in $( find . ) ; do
+	local files="$( find . -type f )"
+
+	for f in $files ; do
 		print red "$f"
 		print " -> "
 		print green "${f//$change_from/$change_to}\n"
 	done
 
 	if ask_user 'Is this correct?' ; then
-		for f in $( find . ) ; do
+		for f in $files ; do
 			mv -v "$f" "${f//$change_from/$change_to}"
 		done
 	fi
