778563ad8ac6c1e6604254e6294d0d2205ffb6f6
Peter Kiss <ypetya@gmail.com>
2016-05-25 15:17:23 +0200

    'first' accepts param

--- first.sh
+++ first.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # returns the first $1 lines of the stream
+# it can pass a filename too
+# E.g: print the first 3 lines of <filename>:
+# $ first 3 filename
+#
 function first() {
+	local input=( $@ )
 	local lines=${1:-1}
-	head -n "$lines"
+	head -n "$lines" "${input[@]:1}"
 }
