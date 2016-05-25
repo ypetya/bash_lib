e23494a4bcb9e940c349e4d592c425a3c98c2b79
Peter Kiss <ypetya@gmail.com>
2016-05-25 15:18:15 +0200

    rename_files add blacklist
    (still need to add filter)

--- rename_files.sh
+++ rename_files.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+import find_blacklist
+
 function rename_files() {
 	local change_from="${1? param missing - change_from}"
 	local change_to="${2? param missing - change_to}"
 
-	local files="$( find . -type f )"
+	local files="$( find . -type f $find_blacklist )"
 
 	for f in $files ; do
+		# FIXME should introduce filter for files
 		print red "$f"
 		print " -> "
 		print green "${f//$change_from/$change_to}\n"
