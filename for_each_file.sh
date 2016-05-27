f7bc520a4306df240d91bd0f3b92edbc1ee121c4
Peter Kiss <ypetya@gmail.com>
2016-05-27 16:36:10 +0200

    for_each_file js_get_templates

---
+++ for_each_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+import debug
+import find_blacklist
+
+# it will call $1 callback with file names passing one-by-one
+# makes as many calls as files exists
+#
+# Example:
+# -------
+# for_each_file get_file_type
+# => will print out all the file types
+function for_each_file() {
+	local callback="${1?param missing - callback}"
+	local files=( $( eval find . -type f $find_blacklist ) )
+	
+	for file in ${files[@]} ; do
+		debug "$callback $file"
+		$callback "$file"
+	done
+}
---
+++ js_get_templates.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+import list_imports_of_file
+
+function js_get_templates() {
+	local file="${1?param missing - file}"
+	list_imports_of_file "$file" | sed -rn "s/^text!(.*)$/\1/p"
+}
