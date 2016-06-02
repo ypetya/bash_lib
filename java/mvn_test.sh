532a72d1c61b5d7b3778a6877c3d98c7e2f9c2bf
Peter Kiss <ypetya@gmail.com>
2016-06-02 18:00:38 +0200

    mvn_test

---
+++ java/mvn_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function java.mvn_test() {
+	require_package mvn
+	local test_filter_patter="${1?param missing test_filter_pattern}"
+	#type mvn
+	mvn -X -Dtest="$test_filter_pattern" test
+}
+
+alias mvn_test='java.mvn_test'
