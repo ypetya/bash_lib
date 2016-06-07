e4b5d5eddb373e2302c009acb6befa5437e7a03d
Peter Kiss <ypetya@gmail.com>
2016-06-07 10:35:18 +0200

    add js build paths to grep blacklist

--- grep_directory_blacklist.sh
+++ grep_directory_blacklist.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-export grep_directory_blacklist="--exclude-dir='.git' --exclude-dir='.svn'"
+export grep_directory_blacklist="--exclude-dir='.git' \
+--exclude-dir='dist' \
+--exclude-dir='coverage' \
+--exclude-dir='.svn'"
