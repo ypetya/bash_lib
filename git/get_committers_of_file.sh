7914a648f33e960580d8db47c9c3386168c3c691
Peter Kiss <ypetya@gmail.com>
2016-06-13 16:43:05 +0200

    fix get_committers_of_file

--- git/get_committers_of_file.sh
+++ git/get_committers_of_file.sh
@@ -6,5 +6,5 @@ function git.get_committers_of_file() {
 	local file_name=${1? param missing - file_name}
 
 	git blame "$file_name" -e -w | \
-		sed -r "$sed.git_extract_commiter"
+		sed -r "$sed_git_extract_commiter"
 }
