ccd542aa361bf66458195d71a93aeb475100c832
Peter Kiss <ypetya@gmail.com>
2016-06-18 11:51:40 +0200

    introduce http_get status code handling
    * return status is 1, when status code starts with non 2
    * curl has now a transfer timeout

--- http_get.sh
+++ http_get.sh
@@ -3,22 +3,27 @@
 function http_get() {
 	# set default connect timeout to 5 seconds
 	local HTTP_CONNECT_TIMEOUT_IN_SEC=${HTTP_CONNECT_TIMEOUT_IN_SEC:-5}
+	# set default transfer timeout to 10 seconds
+	local HTTP_TRANSFER_TIMEOUT_IN_SEC=${HTTP_TRANSFER_TIMEOUT_IN_SEC:-10}
 	# -s is for silent mode ( not to output timings)
 	# -L is for follow redirects
 	# finally we dont want to output errors
+	local response=$( curl -sL -w "%{http_code}" \
+		--connect-timeout $HTTP_CONNECT_TIMEOUT_IN_SEC \
+		-m $HTTP_TRANSFER_TIMEOUT_IN_SEC "$@" \
+		2> /dev/null )
+	# I could use a while to parseall the headers, but this way is
+	# much more simple to get the status code only
+	local status="${response:${#response}-3}"
+	local body=''
+	if [ ${#response} -gt 3 ]; then
+	  body="${response:0:${#response}-3}"
+	fi
 
-	# TODO fix status code check
-	#creates a new file descriptor 3 that redirects to 1 (STDOUT)
-	#exec 3>&1
-	# Run curl in a separate command, capturing output of -w "%{http_code}"
-	# into HTTP_STATUS
-	# and sending the content to this command's STDOUT with -o >(cat >&3)
-	#local HTTP_STATUS=$(
-	#	curl -sL --connect-timeout $HTTP_CONNECT_TIMEOUT_IN_SEC \
-	#	-w "%{http_code}" -o >(cat >&3) "$@" 2> /dev/null)
-	# return status
-	#if [ ! "$HTTP_STATUS" = "200" ] ; then
-	#	return $HTTP_STATUS
-	#fi
-	curl -sL --connect-timeout $HTTP_CONNECT_TIMEOUT_IN_SEC "$@" 2> /dev/null
+	echo "$body"
+
+	# return err when not classed OK
+	if [ ! "${status:0:1}" = "2" ] ; then
+		return 1
+	fi
 }
