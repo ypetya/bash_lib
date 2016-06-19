07b08e5d238d0ea69dd0378729738cfdba0ab682
Peter Kiss <ypetya@gmail.com>
2016-06-19 17:08:04 +0200

    user_config chrome : update for linux

--- load_user_config.sh
+++ load_user_config.sh
@@ -4,6 +4,8 @@ import file.is_file
 import ask_user
 import print.print
 import file.find_first
+import is_mingw
+import require_package
 
 function load_user_config() {
 	if ! is_file ~/.user_config ; then
@@ -13,10 +15,15 @@ function load_user_config() {
 		echo "service_endpoint=\"$service_endpoint\"" > ~/.user_config
 		ask_user "Please enter token" token
 		echo "token=\"$token\"" >> ~/.user_config
-		print "Searching for chrome in /c/..\n"
-		# TODO is_mingw
-		echo "chrome_exe=\"$(file.find_first /c/ chrome.exe)\"" >> \
-		~/.user_config
+
+		print "Searching for chrome..\n"
+		if is_mingw ; then
+			echo "chrome_exe=\"$(file.find_first /c/ chrome.exe)\"" >> \
+			~/.user_config
+		else
+			require_package chromium-browser
+			echo "chrome_exe=\"$(which chromium-browser)\"" >> ~/.user_config
+		fi
 	fi
 	source ~/.user_config
 }
