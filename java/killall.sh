8ae84c5d5f42a89fb0c0884ebd99ada504b58ac9
Peter Kiss <ypetya@gmail.com>
2016-06-23 18:31:25 +0200

    java.kill_all move require_pkg inside

--- java/killall.sh
+++ java/killall.sh
@@ -2,13 +2,13 @@
 
 import require_package
 
-if require_package jps taskkill ; then
-	function java.killall() {
-	  unset PIDS
-	  local PIDS=( $(jps | cut -d' ' -f1) )
-	  PIDS=(${PIDS[@]:1})
-	  for pid in $PIDS; do
+function java.killall() {
+	require_package jps taskkill
+	unset PIDS
+	local PIDS=( $(jps | cut -d' ' -f1) )
+	PIDS=(${PIDS[@]:1})
+	for pid in $PIDS; do
 		taskkill //f //pid $pid
-	  done
-	}
-fi
+	done
+}
+
