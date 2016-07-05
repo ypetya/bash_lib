e7b820a9330dc9c314d3a8700eb45dd39a888a9c
Peter Kiss <ypetya@gmail.com>
2016-07-05 10:31:48 +0200

    enable git.who_have_done_this to work on uncommitted git-tree

--- git/who_have_done_this.sh
+++ git/who_have_done_this.sh
@@ -4,9 +4,12 @@ import git.grep list_counts_desc timer git.changed
 
 function git.who_have_done_this() {
 	if git.changed ; then
-		error 'Please commit your changes'
-		return 1
-	  fi
+		if ! ask_user 'You have uncommited changes. Do you want to continue?'
+		then
+			error 'Please commit your changes'
+			return 1
+		fi
+	fi
 	timer
 	git.grep $@ | cut -d ' ' -f 2 | list_counts_desc
 	timer_stop
