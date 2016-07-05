29907eeefee4aa52eb9c504216c437f94ad17f79
Peter Kiss <ypetya@gmail.com>
2016-07-05 10:30:22 +0200

    use "git grep" to fasten up things

--- git/grep.sh
+++ git/grep.sh
@@ -2,29 +2,28 @@
 
 import print.print git.get_committers_of_file
 
+# TODO use git grep it's much faster
 function git.grep() {
 	local file_name
 	local last_file_name='-1'
-	local change_line
-	local matching_line
-	local committers
-	# -i insensitive
-	# -r recursive
-	# -n containing line numbers
-	grep -irn "$@" $(git ls) | while read match ; do
-		file_name="$(echo $match | cut -d ':' -f 1)"
-		change_line="$(echo $match | cut -d ':' -f 2)"
-		matching_line="$(echo $match | cut -d ':' -f 3,4,5)"
-		if [ ! "$file_name" == "$last_file_name" ] ; then
-			committers=( $(git.get_committers_of_file "$file_name") )
-		fi
+	local change_line matching_line committers match
 
-		last_file_name="$file_name"
-		print yellow "$file_name"
-		print ":$change_line "
-		print green "${committers[$change_line]}"
-		print ' '
-		echo "$matching_line"
+	git grep -ni "$@" | while read match ; do
+		if (( ${#match} > 0 )) ; then
+			file_name="$(echo $match | cut -d ':' -f 1)"
+			change_line="$(echo $match | cut -d ':' -f 2)"
+			matching_line="$(echo $match | cut -d ':' -f 3,4,5,6)"
+			if [ ! "$file_name" == "$last_file_name" ] ; then
+				committers=( $(git.get_committers_of_file "$file") )
+			fi
+
+			last_file_name="$file"
+			print yellow "$file"
+			print ":$change_line "
+			print green "${committers[$change_line]}"
+			print ' '
+			echo "$matching_line"
+		fi
 	done
 }
 
