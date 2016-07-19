65843d33a50a600ab26b1d9c617e828147b1fc9e
Peter Kiss <ypetya@gmail.com>
2016-07-19 17:02:12 +0200

    jira command is now supporting new options
    * without paramterers it opens the jira_dashboard url in chrome
    * passing a ticket number it opens the issue
    * passing a number only it appends it to jira_project

--- jira.sh
+++ jira.sh
@@ -2,14 +2,18 @@
 
 import chrome
 import user.load_config
-import string.is_empty
+import string
 
 function jira() {
 	local ticket_id="$1"
 	user.load_config
+
 	if string.is_empty "$ticket_id" ; then
 		chrome open "${jira_dashboard_url}"
 	else
+		if string.match_regex "$ticket_id" '^[0-9]+$' ; then
+			ticket_id="${jira_project}-${ticket_id}"
+		fi
 		chrome open "${jira_url/%ticket_id/$ticket_id}"
 	fi
 }
