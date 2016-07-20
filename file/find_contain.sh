9cc9949cc860635e7fc111ae843e842cc3e8b3df
Peter Kiss <ypetya@gmail.com>
2016-07-20 18:24:16 +0200

    file.find_contain

---
+++ file/find_contain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+import find.blacklist
+
+function file.find_contain() {
+	local file_pattern="${1? param missing - file_pattern}"
+	local string="${2? param missing - string}"
+	find.blacklist . -type f -name "${file_pattern}" -print0 |
+	xargs -0 grep "$string" --color
+}
--- file/find_not_contain.sh
+++ file/find_not_contain.sh
@@ -2,7 +2,7 @@
 
 import grep.blacklist
 
-function file_find_not_contain() {
+function file.find_not_contain() {
 	local file_pattern="${1? param missing - file_pattern}"
 	local string="${2? param missing - string}"
 	grep.blacklist -rL --include='$file_pattern' '$string'
