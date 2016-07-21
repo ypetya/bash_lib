0947b2ba773d60331534e6c932393a13a31e345d
Peter Kiss <ypetya@gmail.com>
2016-07-21 16:36:39 +0200

    enhance idea.open_matching to print a grep result first and some stats

--- idea/open_matching.sh
+++ idea/open_matching.sh
@@ -7,8 +7,12 @@ import user.load_config
 function idea.open_matching() {
 	local pattern="${1?param missing -pattern to find for edit}"
 	user.load_config
+	search "$pattern"
 	print 'Total matches '
-	print yellow "$(grep.blacklist -rc "$pattern" )\n"
+	print yellow "$(grep.blacklist -r "$pattern" | wc -l)"
+	print ' in '
+	print yellow  "$(grep.blacklist -rl "$pattern" | wc -l)"
+	print ' files\n'
 	for file in $(grep.blacklist -rl "$pattern" ) ; do
 		if ask_user "Do you want to edit $file?" ; then
 			eval "$idea_exe $file"
