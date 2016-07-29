a504d7ea6e308c30c9b9bc360fd7d678e601003e
Peter Kiss <ypetya@gmail.com>
2016-07-29 15:16:11 +0200

    extend npm.fix

--- npm/fix.sh
+++ npm/fix.sh
@@ -1,7 +1,11 @@
 import require_package
+import print.print
 
 function npm.fix() {
   require_package npm
   npm config ls -l | grep $USER
-  echo ' * Fix these config variables!'
+  print yellow 'Should point to a local dir:\n'
+  npm config get cache
+  print 'npm config set cache <local_abs_path> --global\n'
+  print yellow ' * Fix these config variables!\n'
 }
