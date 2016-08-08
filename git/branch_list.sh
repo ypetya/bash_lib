ae57a3c4371106d0df4d11b4a2f5250c8356308e
Kiss Peter <ypetya@gmail.com>
2016-08-08 18:27:06 +0200

    git.branch_list by most recent created branch

--- git/branch_list.sh
+++ git/branch_list.sh
@@ -3,12 +3,13 @@
 # --list shows local branches
 # -r shows remotes
 # -a shows both remotes and local
+# --sort=key : see git help git-for-each-ref for keys
 # <pattern> is a shell pattern
 function git.branch_list() {
 	if (( $# > 0 )) ; then
 		local pattern="*${1}*"
-		git branch --list -a $pattern
+		git branch --list -a --sort='-creatordate' $pattern
 	else
-		git branch --list -a
+		git branch --list -a --sort='-creatordate'
 	fi
 }
