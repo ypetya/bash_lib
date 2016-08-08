e0dd1843e446d7376bd198c667312c021a9cbb37
Kiss Peter <ypetya@gmail.com>
2016-08-08 18:30:45 +0200

    git.checkout for chosing the most recent branch / track it and change it onto it

---
+++ git/checkout.sh
@@ -0,0 +1,33 @@
+function git.checkout() {
+	set -f
+	local branch_pattern="*${1?param missing - branch_pattern}*"
+	local matching
+	local changed=0
+	if git.changed &&
+		ask_user 'You have local changes. Do you want to continue?'
+	then
+		changed=1
+		git stash
+	fi
+	print yellow 'Please select which branch to checkout?\n'
+	select matching in $( git branch \
+		--list -a --no-color \
+		--sort='-creatordate' \
+		"$branch_pattern" | tr -d '* ' )
+	do
+		if [ "${matching:0:8}" == 'remotes/' ] ; then
+			print yellow 'Creating local branch for remote branch...\n'
+			git checkout $matching -b "${matching:8}" --track
+		else
+			print yellow 'Checking out local branch...\n'
+			git checkout $matching
+			git pull --stat
+		fi
+
+		if [[ changed == 1 ]] ; then
+			git stash pop
+		fi
+		set +f
+		return 0
+	done
+}
