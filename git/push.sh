5aa93e5750b91c6b3b13f98a96e581c1ca89ef2c
Peter Kiss <ypetya@gmail.com>
2016-08-11 15:28:16 +0200

    git.push : just open the stash_url for branches it's easier for now

--- git/push.sh
+++ git/push.sh
@@ -3,22 +3,11 @@ import user.ask
 import chrome
 
 function git.push() {
-	local PR_link=""
+	git push $@
 
-	while read -r line ; do
-		echo ">$line"
-
-		if [ "${line:0:15}" == "remote:   https" ] ; then
-			PR_link="${line:10}"
-			print green ' * PR link detected!\n'
-		fi
-	done <<EOT
-	$( git push --porcelain $@ )
-EOT
-
-	if (( ${#PR_link} >0 )) && user.ask 'Would you like to open PR?'
+	if (( ${#stash_url} >0 )) && user.ask 'Would you like to open in browser?'
 	then
 		print yellow ' * Opening browser...\'
-		chrome open "$PR_link"
+		chrome open "$stash_url"
 	fi
 }
