644e38bbc8e8a4643979ac41a801fc676a4dc246
Peter Kiss <ypetya@gmail.com>
2016-08-11 15:28:34 +0200

    user.config.edit now supports adding new key-value pair

--- user/config/edit.sh
+++ user/config/edit.sh
@@ -3,27 +3,47 @@ import user.ask
 import set_separator
 
 function user.config.edit() {
-	local line key value
+	local line
 	set_separator
-	select line in $(cat ~/.user_config | sort) Quit
+	select line in $(cat ~/.user_config | sort) "New key-value pair" Quit
 	do
-		if [ "$line" == "Quit" ] ; then
-			echo bye
-			reset_separator
-			return
-		fi
-		key="$( echo $line | cut -d '=' -f 1 )"
-		value="$( echo $line | cut -d '=' -f 2,3,4,5,6,7,8 )"
+		case "$line" in
+			"New key-value pair")
+				user.config.edit.new
+				;;
+			Quit)
+				echo bye
+				;;
+			*)  user.config.edit.edit "$line"
+				;;
+		esac
 
-		user.config.edit.print_kv "$key" "$value"
-
-		user.ask 'New value (without quotes)?' value
-
-		sed -ie "s/$key=.*/$key=\"$value\"/" ~/.user_config
 		user.config.load
 		reset_separator
 		return
 	done
+}
+
+function user.config.edit.new() {
+	local key value
+	user.ask 'New key (without quotes)?' key
+	user.ask 'New value (without quotes)?' value
+	user.config.edit.print_kv "$key" "$value"
+
+	echo "$key=\"$value\"" >> ~/.user_config
+}
+
+function user.config.edit.edit() {
+	local key value
+	local line="$1"
+	key="$( echo $line | cut -d '=' -f 1 )"
+	value="$( echo $line | cut -d '=' -f 2,3,4,5,6,7,8 )"
+
+	user.config.edit.print_kv "$key" "$value"
+
+	user.ask 'New value (without quotes)?' value
+
+	sed -ie "s|$key=.*|$key=\"$value\"|" ~/.user_config
 }
 
 function user.config.edit.print_kv() {
