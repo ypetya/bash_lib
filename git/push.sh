05f0b687532db65cd671fb7075d73cae333aa23f
Peter Kiss <ypetya@gmail.com>
2016-08-12 15:48:48 +0200

    fix git.push

--- git/push.sh
+++ git/push.sh
@@ -1,13 +1,17 @@
 import print.print
 import user.ask
+import user.config.load
 import chrome
 
 function git.push() {
-	git push $@
-
-	if (( ${#stash_url} >0 )) && user.ask 'Would you like to open in browser?'
-	then
-		print yellow ' * Opening browser...\'
-		chrome open "$stash_url"
+	if git push $@ ; then
+		user.config.load
+		if (( ${#stash_url} >0 )) ; then
+			if user.ask 'Would you like to open in browser?'
+			then
+				print yellow ' * Opening browser...\n'
+				chrome open "$stash_url"
+			fi
+		fi
 	fi
 }
