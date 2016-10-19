3ac5195ebe13e8bbd008863eb6cacffa7cfacd11
Peter Kiss <ypetya@gmail.com>
2016-10-19 10:30:53 +0200

    add optional key_file name to ssh_setup

--- ssh_setup_public_key.sh
+++ ssh_setup_public_key.sh
@@ -1,11 +1,12 @@
 function ssh_setup_public_key() {
   if [ $# -eq 2 ] ; then
-    cat ~/.ssh/id_rsa.pub | \
+    cat ~/.ssh/${3:-id_rsa}.pub | \
     ssh $1@$2 \
 'mkdir ~/.ssh; touch ~/.ssh/authorized_keys; chmod a=,u=Xrw -R ~/.ssh; \
 cat - >> ~/.ssh/authorized_keys'
   else
-    echo "call this function with \$1=username, \$2=server .\
- This will setup your id_rsa.pub onto ~/.ssh/authorized_keys"
+    echo "call this function with \$1=username, \$2=server \$3=.\
+[key filename defaults to id_rsa]\
+ This will setup your <\$3|id_rsa>.pub onto ~/.ssh/authorized_keys"
   fi
 }
