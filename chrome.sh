c0489cc041c14969905787a8d8755946ce22c8f7
Peter Kiss <ypetya@gmail.com>
2017-03-01 17:53:08 +0100

    change chrome to support opening gmail

--- chrome.sh
+++ chrome.sh
@@ -3,40 +3,43 @@ import print.error
 import string.is_empty
 
 function chrome() {
-	local mode="${1?param missing - mode}"
-	user.config.load
-	local command="$chrome_exe "
-	while (( $# > 0 )) ; do
-		mode="$1"
-		case $mode in
-			dev)
-				mode="--disable-web-security --user-data-dir"
-				;;
-			secure)
-				if string.is_empty "$http_proxy" ; then
-					error "No proxy defined in \$http_proxy"
-					return 1
-				else
-					mode="--proxy-server=$http_proxy"
-				fi
-				;;
-			incognito)
-				mode="--incognito"
-				;;
-			inbox|mail)
-				mode="http://inbox.google.com"
-				;;
-			open)
-				mode=""
-				;;
-			help)
-				echo "try passing arguments dev|secure|incognito|mail|help"
-				return 1
-				;;
-		esac
+  local mode="${1?param missing - mode : dev|secure|incognito|inbox|gmail|open|help}"
+  user.config.load
+  local command="$chrome_exe "
+  while (( $# > 0 )) ; do
+    mode="$1"
+    case $mode in
+      dev)
+        mode="--disable-web-security --user-data-dir"
+        ;;
+      secure)
+        if string.is_empty "$http_proxy" ; then
+          error "No proxy defined in \$http_proxy"
+          return 1
+        else
+          mode="--proxy-server=$http_proxy"
+        fi
+        ;;
+      incognito)
+        mode="--incognito"
+        ;;
+      inbox)
+        mode="http://inbox.google.com"
+        ;;
+      gmail)
+        mode="http://gmail.com"
+        ;;
+      open)
+        mode=""
+        ;;
+      help)
+        echo "try passing arguments dev|secure|incognito|inbox|gmail|open|help"
+        return 1
+        ;;
+    esac
 
-		command="$command $mode"
-		shift
-	done
-	eval $command
+    command="$command $mode"
+    shift
+  done
+  eval $command
 }
