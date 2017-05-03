e9948a1936a35ae23eb581bdd1ce7a27e64ad217
Peter Kiss <ypetya@gmail.com>
2017-05-03 14:25:32 +0200

    fix collect_all_documents for working properly with filenames containing spaces

--- collect_all_documents.sh
+++ collect_all_documents.sh
@@ -1,21 +1,36 @@
-import is_directory
+import file.is_directory
 import find.blacklist
+import set_separator
 
+# Example usage: 
+#
+# 1. create a directory where you want to run the script
+# 2. call the script with the params of the resource target dir, which will be scanned through for docx,mobi,doc,pdf
+# 3. voila, you got all the files
 function collect_all_documents() {
 	local root_dir=${1? param missing - root_dir}
 	if is_directory "$root_dir" ; then
 
-	  files=( $(
-		find.blacklist $root_dir -type f \
-		\( -iname \*.docx \
-		-o -iname \*.mobi \
-		-o -iname \*.doc \
-		-o -iname \*.pdf \)\
-		   2>/dev/null
-		))
+		echo "Collecting documents"
+		echo " from directory : $root_dir"
+		echo " to directory : $PWD"
 
-	  for i in ${files[@]} ; do
-		cp -v $i .
+		set_separator
+	  files=( $(
+		find.blacklist "$root_dir" -type f \
+		-iname '\*.docx' \
+		-o -iname '\*.mobi' \
+		-o -iname '\*.doc' \
+		-o -iname '\*.pdf' 
+		) )
+
+		echo "${#files[@]} documents found."
+
+	  for file in ${files[@]} ; do
+			cp -v "$file" .
 	  done
+		reset_separator
+	else
+		echo "No such directory : $root_dir"
 	fi
 }
