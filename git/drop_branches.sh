d210c4dbd60a789cf7b25625c9e44b8804955f53
Peter Kiss <ypetya@gmail.com>
2017-08-02 16:58:27 +0200

    drop_branches color print

--- git/drop_branches.sh
+++ git/drop_branches.sh
@@ -1,3 +1,4 @@
+import print.print
 import git.drop_local_branch
 import git.drop_remote_branch_at_origin
 
@@ -5,10 +6,10 @@ function git.drop_branches() {
     echo "Removing branches interactive..."
     while (( $# > 0 )) ; do
         local branch="$1"
-        echo " *** Dropping Branch : $branch"
+        print yellow " *** Dropping Branch : $branch\n"
         git.drop_local_branch "$branch"
         git.drop_remote_branch_at_origin "$branch"
         shift
     done
-    echo " *** Done"
+    print yellow " *** Done\n"
 }
