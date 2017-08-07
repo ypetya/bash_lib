db3094ce08230be3eb3c376e1988f89e10b7a421
Peter Kiss <ypetya@gmail.com>
2017-08-07 17:05:35 +0200

    add help text for list_imports

--- file/list_imports.sh
+++ file/list_imports.sh
@@ -2,6 +2,15 @@ import sed.extract_imports
 import file.get_extension
 import sed.extract
 
+#
+# returns a list of imports based on file extension
+#
+# supported extensions
+# --------------------
+#
+# js : es6 style imports
+# sh : bash imports in bash_lib format
+#
 function file.list_imports() {
 	local file="${1? param missing - file}"
 	local extee
