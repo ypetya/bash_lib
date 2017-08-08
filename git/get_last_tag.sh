0919a4652e3f34afa947b4b77f60621bd9bfe136
Peter Kiss <ypetya@gmail.com>
2017-08-08 14:10:58 +0200

    rationalize git.get_last_tag and git.tags by alterint sort

--- git/get_last_tag.sh
+++ git/get_last_tag.sh
@@ -3,5 +3,5 @@ import git.tags first
 function git.get_last_tag() {
 	# you can apply more sort=<key> clauses for git tag
 	# the last one gets to be the primary key, or overrides the prev.
-	tags --sort="-version:refname" | first
+	tags --sort="-creatordate" | first
 }
--- git/tags.sh
+++ git/tags.sh
@@ -1,3 +1,9 @@
+# return tags
+#
+# sort order:
+# ----------
+# version:refname  treated as versions
+# -creatordate date descending
 function git.tags() {
 	git tag -l --sort="version:refname" $@
 }
