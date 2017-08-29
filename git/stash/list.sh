5ec85096fbf96a9f69500f13b4b553d093ea6278
Peter Kiss <ypetya@gmail.com>
2017-08-29 14:56:18 +0200

    enable arguments for git.stash.list

--- git/stash/list.sh
+++ git/stash/list.sh
@@ -1,5 +1,5 @@
 # show up stash content, only the human readable part
 
 function git.stash.list() {
-	git stash list | sed -r 's/^[^:]+: (.*)$/\1/'
+	git stash list $@ | sed -r 's/^[^:]+: (.*)$/\1/'
 }
