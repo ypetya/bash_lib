db1ac18551057920cb097458533c40d5abfbd919
Peter Kiss <ypetya@gmail.com>
2017-11-24 10:55:06 +0100

    rebase todo

---
+++ git/merge_with_rebase.sh
@@ -0,0 +1,7 @@
+# take two branch patterns : branch A, branch B. 
+# Execute the following steps:
+# Update from remotes
+# Let the user to select branches
+# Interactive rebase B on A
+# checkout A, merge with fast forward ( ~ is it equal with a reset ? )
+# if no branch specified A can be the current branch
