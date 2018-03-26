a6f9cb5061d16b930ea0d43f172121566876e67a
Peter Kiss <ypetya@gmail.com>
2018-03-26 17:10:53 +0200

    fix typo

--- npm/start_http_server.sh
+++ npm/start_http_server.sh
@@ -3,6 +3,6 @@ import npm.require_global
 # example call to use directory browse, CORS and open browser:
 # http-server -do --cors
 function npm.start_http_server() {
-	npm_require_global http-server
+	npm.require_global http-server
 	~/http-server $@
 }
